77880c0a848ee1ce9d6ae1417a967379097ab1f7
Sam Byrd <sambyrd815@gmail.com>
2016-12-07 20:29:11 -0500

    save

--- dogedodge/dodge.js
+++ dogedodge/dodge.js
@@ -4,8 +4,21 @@ class Boot {
      this.scale.pageAlignHorizontally = true;
      this.scale.pageAlignVertically = true;
                     }
+create() {
+      this.state.start("Load")
+}
+}
+class Load {
+    preload() {
+          console.log("Loading...");
+      }
+              create() {
+                    console.log("Loaded");
+                      }
 }
 var game = new Phaser.Game(320,568);
 
-game.state.add("Boot",Boot);
+game.state.add("Boot",Boot)
+game.state.add("Load",Load);
 game.state.start("Boot");
+
